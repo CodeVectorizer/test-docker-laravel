3694641c6685d5f803bc898f6ed53acd609a1ae8
CodeVectorizer <lukmanafandi10@gmail.com>
2023-09-27 20:55:06 +0700

    run

---
+++ script-dev.sh
@@ -0,0 +1,32 @@
+# Composer
+composer update
+composer install
+composer dump-autoload
+
+# Laravel
+php artisan migrate
+php artisan db:seed
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan optimize
+
+# print cool message to console like "Completed" but bigger
+echo -e "\n\
+    _____ _
+    / ____| |
+    | |    | |__   ___  ___ ___
+    | |    | '_ \ / _ \/ __/ __|
+    | |____| | | |  __/\__ \__ \
+    \_____|_| |_|\___||___/___/
+    "
+
+
+
+
+
+
